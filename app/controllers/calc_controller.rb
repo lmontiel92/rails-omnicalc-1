63aff44486286250d22b005c586e7b22dfc97889
lmontiel92 <146214091+lmontiel92@users.noreply.github.com>
2023-11-07 05:40:11 +0000

    square root functionality

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -6,4 +6,14 @@ class CalcController < ApplicationController
 
   end
 
+
+  def calc_square
+    
+    @user_square_input = params.fetch("user_number").to_f
+    @user_square_result = @user_square_input**2
+
+    render ({:template => "view_templates/square_results"})
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   
   get("/", {:controller => "calc", :action => "show_initial_page"})
 
+  get("/square/new", {:controller => "calc", :action => "show_initial_page"})
+
+  get("/square/results", {:controller => "calc", :action => "calc_square"})
+
   
 
 end
